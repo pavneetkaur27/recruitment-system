f4852e1d442ac5873a933a343455317c50ff094d
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-05-11 15:19:21 +0530

    + build created

---
+++ recuritment-frontend/build/precache-manifest.72500c225821f347c9c491b0be24b9dc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce1c52b676a8512b89fea0aac4e8253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "945fd63a01ac9c194f23",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "c466692407e149739b4a",
+    "url": "/static/css/main.76056ea9.chunk.css"
+  },
+  {
+    "revision": "945fd63a01ac9c194f23",
+    "url": "/static/js/2.bb94b1b1.chunk.js"
+  },
+  {
+    "revision": "c466692407e149739b4a",
+    "url": "/static/js/main.d76f7462.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f84ed13bd0135852e888c22969d0b775",
+    "url": "/static/media/candidate_profile_pic.f84ed13b.svg"
+  },
+  {
+    "revision": "b3ca7e94182fbc18b9fea69286c17621",
+    "url": "/static/media/chip-close-icon.b3ca7e94.svg"
+  },
+  {
+    "revision": "3d388d912b96aa51bdd817f74382ab14",
+    "url": "/static/media/close-btn.3d388d91.svg"
+  },
+  {
+    "revision": "036a4c2ed9dcf3faf11734f5eb6241ba",
+    "url": "/static/media/dropdown-arrow.036a4c2e.svg"
+  },
+  {
+    "revision": "6fe4423785c5a745b889bd048bf39a96",
+    "url": "/static/media/fail.6fe44237.svg"
+  },
+  {
+    "revision": "d7e612e0c49b4927ae4549d9b9177f8d",
+    "url": "/static/media/ico_users.d7e612e0.svg"
+  },
+  {
+    "revision": "707852f13145a7f5606b4a83505faf6f",
+    "url": "/static/media/loader.707852f1.svg"
+  },
+  {
+    "revision": "85092b9762ff6383e3f64ab63f5cd6da",
+    "url": "/static/media/success.85092b97.svg"
+  }
+]);
